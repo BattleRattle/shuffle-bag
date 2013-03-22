b71d17d6c22ffe140e5b79fbe5c69d7f045e2781
Norman Soetbeer <norman.soetbeer@gmail.com>
2013-03-22 19:22:12 +0100

    cleaned up example

--- examples/random_item.php
+++ examples/random_item.php
@@ -6,7 +6,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Item IDs with their probabilites to be chosen
+use BattleRattle\ShuffleBag\ShuffleBag;
+
+// Item IDs with their probabilities to be used
 $items = array(
     1 => 0.5,
     2 => 0.2,
@@ -16,29 +18,12 @@ $items = array(
     6 => 0.05,
 );
 
-$shuffleBag = new \BattleRattle\ShuffleBag\ShuffleBag();
+$shuffleBag = new ShuffleBag();
 
 // Since we use decimal numbers with 2 decimal places, we will multiply the occurrences by 100.
 foreach ($items as $itemId => $probability) {
     $shuffleBag->add($itemId, $probability * 100);
 }
 
-//$randomItemId = $shuffleBag->next();
-//var_dump($randomItemId);
-
-$stats = array(
-    1 => 0,
-    2 => 0,
-    3 => 0,
-    4 => 0,
-    5 => 0,
-    6 => 0
-);
-
-for ($i = 0; $i < 1000; $i++) {
-    $stats[$shuffleBag->next()]++;
-}
-
-foreach ($stats as $id => $count) {
-    echo sprintf('%d: %5d', $id, $count) . PHP_EOL;
-}
+$randomItemId = $shuffleBag->next();
+var_dump($randomItemId);
